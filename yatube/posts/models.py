f300ddf51b6c2441d899faa64547343f790cc3ff
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-24 07:40:12 +0300

    fix the comments of the reviewer

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,15 +4,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-# Create your models here.
-
-
 class Group(models.Model):
     title = models.CharField(max_length=200,
-                             verbose_name='Название сообщества')
-    slug = models.SlugField(max_length=200, unique=True, verbose_name='Адрес')
+                             verbose_name="Название сообщества")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="Адрес")
     description = models.TextField(null=True, blank=True,
-                                   verbose_name='Описание')
+                                   verbose_name="Описание")
 
     def __str__(self):
         return self.title
@@ -20,12 +17,12 @@ class Group(models.Model):
 
 class Post(models.Model):
     group = models.ForeignKey(Group, null=True, blank=True,
-                              verbose_name='Сообщество',
+                              verbose_name="Сообщество",
                               related_name="posts",
-                              on_delete=models.CASCADE)
+                              on_delete=models.SET_NULL)
     text = models.TextField(verbose_name="Текст поста")
     pub_date = models.DateTimeField(auto_now_add=True,
-                                    verbose_name='Дата публикации')
+                                    verbose_name="Дата публикации")
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="posts",)
 
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/<str:slug>", views.group_posts, name="group")
+    path("group/<str:slug>/", views.group_posts, name="group")
 ]
