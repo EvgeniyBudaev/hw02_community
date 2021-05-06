9f2612d8b51fad940e0781fc24a3682d8c4ed098
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-05-06 18:01:30 +0300

    Доработка базового шаблона

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+import datetime as dt
 
 from .models import Post, Group
 
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "index.html", {"posts": latest})
+    current_year = dt.datetime.now().year
+    return render(request, "index.html", {"posts": latest, "year": current_year})
 
 
 def group_posts(request, slug):
