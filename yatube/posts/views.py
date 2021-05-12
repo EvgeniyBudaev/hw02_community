ff7388b6d12f9e292de7e8eb69b4eeb47b03ca11
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-05-12 06:59:56 +0300

    add about page and pagination

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 import datetime as dt
 
@@ -5,12 +6,18 @@ from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
     current_year = dt.datetime.now().year
-    return render(request, "index.html", {"posts": latest, "year": current_year})
+    return render(request, "index.html", {"page": page, "year": current_year})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(request, "group.html", {"group": group, "posts": posts})
+    posts_list = Post.objects.filter(group=group).order_by("-pub_date")
+    paginator = Paginator(posts_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, "group.html", {"group": group, "page": page})
